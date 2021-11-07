cb51da9cf148bd47b5e96442af76322c10f54ccf
sbx_user1051 <sbx_user1051@169.254.36.141>
2021-11-07 10:00:24 +0000

    Sun Nov 07 10:00:24 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -20,27 +20,39 @@ class Checkout:
     ERROR_RETURN_CODE = -1
 
     def __init__(self, prices, offers):
-        self.prices = prices
-        self.offers = offers
+        self.prices_by_sku = prices
+        self.offers_by_sku = { offer.sku: offer for offer in offers }
 
     def checkout(self, skus: str) -> int:
-        if type(skus) != str:
+        try:
+            sku_count = self._validate_and_get_counter(skus)
+        except ValueError:
             return self.ERROR_RETURN_CODE
+
+        prices = [self.prices_by_sku[sku] * count for sku, count in sku_count.items()]
+
+        return sum(prices)
+
+    def get_price_for_sku(self, sku: str) -> int:
+        pass
+
+    def _validate_and_get_counter(self, skus) -> Counter[str]:
+        if type(skus) != str:
+            raise ValueError
 
         sku_count = Counter(skus)
 
         for sku in sku_count:
-            if sku not in self.prices:
-                return self.ERROR_RETURN_CODE
+            if sku not in self.prices_by_sku:
+                raise ValueError
 
-        prices = [self.prices[sku] * count for sku, count in sku_count.items()]
-
-        return sum(prices)
+        return sku_count
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
     return Checkout(SKU_PRICES, OFFERS).checkout(skus)
+
 
 
